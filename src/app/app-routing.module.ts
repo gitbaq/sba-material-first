057fca4a66eee152c796d8ffff3dc0514b79ace0
SBee2020 <gitbaq@gail.com>
2020-09-23 19:47:11 +0300

    Updated Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { CartManagementComponent } from './cart-management/cart-management.component';
+import { MainComponentComponent } from './main-component/main-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponentComponent },
+  { path: 'home', component: MainComponentComponent },
+  { path: 'user', component: UserManagementComponent },
+  { path: 'user/:userId', component: UserManagementComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'cart', component: CartManagementComponent },
+  // { path: 'home',   redirectTo: '/home', pathMatch: 'full' },
+  
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+export class AppRoutingModule { 
+
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OrdersComponent } from './orders/orders.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { CartManagementComponent } from './cart-management/cart-management.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    PageNotFoundComponent,
+    UserManagementComponent,
+    CartManagementComponent,
+    MainComponentComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
